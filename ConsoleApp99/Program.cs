114a1246bffdbb4c7331575f91dc0c64c7e0a775
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-17 16:37:40 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp99/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r1 = 1800, r2 = 2100;
+            string nazwa = @"c:\tmp\lata_przestępne.txt";
+            FileStream plik = new FileStream(nazwa, FileMode.Create, FileAccess.Write, FileShare.None);
+            System.IO.StreamWriter zapis = new StreamWriter(plik);
+            for (int i=r1; i<=r2; i++)
+            {
+                if (((i%4==0)&&(i%100!=0))||(i%400==0))
+                {
+                    
+                    zapis.WriteLine(i);
+                    
+                }
+            }
+            zapis.Close();
+            plik.Close();
+        }
+    }
+}
